a2dc3e8f4aad7bee30ad2512303b5289f5493fb4
Jerome Marousez <jeromedoubleyou@gmail.com>
2022-04-29 15:01:00 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
+
+<!--  <div>{{map.metadata}}</div>-->
+<!--  <div v-for="(room, id) in map.data" :key="id">-->
+<!--    <div style="margin-bottom: 20px;">x: {{room.x}} / y: {{room.y}}</div>-->
+
+<!--    <div v-for="(cell, id) in room.roomData" :key="id">-->
+<!--      <div>{{cell}}</div>-->
+<!--    </div>-->
+
+<!--  </div>-->
+
+
 </template>
 
+
+<script>
+export default {
+  name: 'app',
+
+  provide() {
+    return {
+      tileSize: 16,
+      displayTileSize: 48,
+      roomRows: this.roomRows,
+      roomCols: this.roomCols,
+      mapRows: this.mapRows,
+      mapCols: this.mapCols,
+      currentRoom: this.currentRoom,
+      tileSetCols: this.tileSetCols,
+      tileSetRows: this.tileSetRows,
+      metadata: this.metadata,
+      tileSet: this.tileSet,
+      currentRoomTileSet: this.currentRoomTileSet,
+      level: this.level,
+    }
+  },
+
+  data(){
+    return {
+      roomRows: 4,
+      roomCols: 4,
+      mapRows: 2,
+      mapCols: 2,
+      metadata: [],
+      tileSetCols: 4,
+      tileSetRows: 16,
+      level: 1,
+      tileSet: "resources/tilesets/A.png",
+      currentRoom: {
+        x: 1,
+        y: 1
+      },
+      currentRoomTileSet: [],
+    }
+  },
+
+  computed: {
+    tileSetGridSize(){
+      return this.tileSetCols * this.tileSetRows
+    }
+  },
+
+  methods:{
+
+  },
+
+  created(){
+    let x = 0
+    let y = 0
+    for(let i = 0; i < this.tileSetGridSize; i++){
+      if(x > 3) {
+        x = 0
+        y++
+      }
+      this.metadata.push({
+        x: x,
+        y: y,
+        isWalkable: false,
+        isDoor: false,
+        interactive: 0,
+      })
+      x++
+    }
+
+
+  },
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './styles/style.css';
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,122 @@
 <template>
+
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <MapToolBar v-if="currentTab === 'mapEditor'" />
+
+    <MetaDataToolBar v-if="currentTab === 'metadataEditor'">
+      <template v-slot:metadata>
+        <MetadataOverlay />
+      </template>
+      <template v-slot:default>
+        <Tileset :source="tileSet" />
+      </template>
+    </MetaDataToolBar>
+
+    <div>
+      <Map />
+
+      <div style="margin-top: 20px; display: flex">
+        <div>
+          <div class="color" style="background-color: hsl(350, 50%, 40%)" />
+          <div class="color" style="background-color: hsl(350, 50%, 50%)" />
+          <div class="color" style="background-color: hsl(350, 50%, 60%)" />
+        </div>
+
+        <div>
+          <div class="color" style="background-color: hsl(20, 40%, 40%)" />
+          <div class="color" style="background-color: hsl(20, 40%, 50%)" />
+          <div class="color" style="background-color: hsl(20, 40%, 60%)" />
+        </div>
+
+        <div>
+          <div class="color" style="background-color: hsl(130, 50%, 35%)" />
+          <div class="color" style="background-color: hsl(130, 50%, 45%)" />
+          <div class="color" style="background-color: hsl(130, 50%, 55%)" />
+        </div>
+
+        <div>
+          <div class="color" style="background-color: hsl(105, 40%, 40%)" />
+          <div class="color" style="background-color: hsl(105, 40%, 50%)" />
+          <div class="color" style="background-color: hsl(105, 40%, 60%)" />
+        </div>
+
+        <div>
+          <div class="color" style="background-color: hsl(200, 50%, 40%)" />
+          <div class="color" style="background-color: hsl(200, 50%, 50%)" />
+          <div class="color" style="background-color: hsl(200, 50%, 60%)" />
+        </div>
+
+        <div>
+          <div class="color" style="background-color: hsl(220, 40%, 40%)" />
+          <div class="color" style="background-color: hsl(220, 40%, 50%)" />
+          <div class="color" style="background-color: hsl(220, 40%, 60%)" />
+        </div>
+
+        <div>
+          <div class="color" style="background-color: hsl(200, 0%, 40%)" />
+          <div class="color" style="background-color: hsl(200, 0%, 65%)" />
+          <div class="color" style="background-color: hsl(200, 0%, 85%)" />
+        </div>
+
+        <div class="color" style="background-color: hsl(200, 0%, 25%)" />
+
+
+      </div>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Tileset from "@/components/Tileset"
+import Map from "@/components/Map"
+import MapToolBar from "@/components/MapToolBar"
+import MetaDataToolBar from "@/components/MetaDataToolBar"
+import MetadataOverlay from "@/components/MetadataOverlay"
+import Room from "@/components/Room"
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    MapToolBar,
+    MetaDataToolBar,
+    Tileset,
+    MetadataOverlay,
+    Map,
+    Room,
+  },
+
+  inject: ['tileSize', 'tileSet'],
+
+  data() {
+    return {
+      currentTab: 'mapEditor',
+    }
+  },
+
+  computed: {
+  },
+
+  methods:{
+  },
+  mounted(){
+  },
+  created(){
+  },
 }
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.color {
+  width: 10px;
+  height: 10px;
+}
+</style>
